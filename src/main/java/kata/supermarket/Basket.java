9eb27d52c9398e15adf33435cdfcf2a92f9a62db
sarpreetsingh <sunnybuttar31@gmail.com>
2022-04-14 09:42:48 +0100

    integrate discount rule with basket

--- src/main/java/kata/supermarket/Basket.java
+++ src/main/java/kata/supermarket/Basket.java
@@ -7,9 +7,11 @@ import java.util.List;
 
 public class Basket {
     private final List<Item> items;
+    private final DiscountRule discountRule;
 
-    public Basket(final List<Item> items) {
+    public Basket(final List<Item> items, final DiscountRule discountRule) {
         this.items = items;
+        this.discountRule = discountRule;
     }
 
     public void add(final Item item) {
@@ -38,15 +40,9 @@ public class Basket {
                     .setScale(2, RoundingMode.HALF_UP);
         }
 
-        /**
-         * TODO: This could be a good place to apply the results of
-         *  the discount calculations.
-         *  It is not likely to be the best place to do those calculations.
-         *  Think about how Basket could interact with something
-         *  which provides that functionality.
-         */
         private BigDecimal discounts() {
-            return BigDecimal.ZERO;
+            return discountRule.getDiscount(items)
+                    .setScale(2, RoundingMode.HALF_UP);
         }
 
         private BigDecimal calculate() {
--- src/test/java/kata/supermarket/BasketTest.java
+++ src/test/java/kata/supermarket/BasketTest.java
@@ -9,6 +9,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,7 +20,7 @@ class BasketTest {
     @MethodSource
     @ParameterizedTest(name = "{0}")
     void basketProvidesTotalValue(String description, String expectedTotal, Iterable<Item> items) {
-        final Basket basket = new Basket(new ArrayList<>());
+        final Basket basket = new Basket(new ArrayList<>(), new DiscountRule(new HashMap<>()));
         items.forEach(basket::add);
         assertEquals(new BigDecimal(expectedTotal), basket.total());
     }
